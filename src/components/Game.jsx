0f292102f75b531ab1ee431b71e7507e0b82d18a
Michael Nguyen <michaelnguy98@gmail.com>
2022-02-01 17:54:34 -0800

    Setup Game component to handle game related components and state

---
+++ src/components/Game.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./Board";
+
+/**
+ * Returns the component that contains all game related components and stores
+ * the current state of the game.
+ *
+ * @returns {JSX.Element} The Game component.
+ */
+export default function Game() {
+  return (
+    <Board />
+  );
+}
