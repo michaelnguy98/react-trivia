d58b8de4322ccab36dc2b95528090eb5f0eaf81c
Michael Nguyen <michaelnguy98@gmail.com>
2022-02-04 19:32:11 -0800

    Add shuffle function for arrays

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -11,6 +11,8 @@ import { getQuestion } from "../api/OpenTriviaAPI";
  */
 export default function Game() {
   const [question, setQuestion] = useState("");
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+  const [answers, setAnswers] = useState([]);
 
   // Get the question, answer, and incorrect answers
   useEffect(() => {
@@ -18,10 +20,19 @@ export default function Game() {
       .then(data => {
         console.log(data);
         setQuestion(data.results[0].question);
+
+        let correct_answer = data.results[0].correct_answer;
+        setCorrectAnswer(correct_answer);
+
+        let possible_answers = data.results[0].incorrect_answers;
+        possible_answers.push(correct_answer);
+        setAnswers(possible_answers);
       })
       .catch(err => {
         console.log(err);
         setQuestion("");
+        setCorrectAnswer(null);
+        setAnswers([]);
       });
   }, []);
 
@@ -30,4 +41,27 @@ export default function Game() {
       <Board question={question} />
     </>
   );
+}
+
+/**
+ * Returns an array that is a shuffled version of the input array.
+ *
+ * @param {Array} array The array to be shuffled.
+ * @returns A new array with the same elements but shuffled.
+ */
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
