3c74f02a18563f4affb391206371bea09cd56e10
Michael Nguyen <michaelnguy98@gmail.com>
2022-02-14 19:02:38 -0800

    Add instruction to StartScreen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -17,6 +17,9 @@ export default function StartScreen(props) {
       transition={{ duration: 1 }}
       exit={{ opacity: 0 }}
     >
+      <motion.h2>
+        Choose the correct answer to the question.
+      </motion.h2>
       <motion.button
         onClick={props.startGame}
         whileHover={{ scale: 1.1 }}
